869e1a08fa3d4a2a25208a8d6ee57621c9e533c9
Ryan <ryanmelopedro190@gmail.com>
2026-08-10 19:23:26 -0300

    feat: implementa estrutura inicial de Pedido

--- src/Pedido.java
+++ src/Pedido.java
@@ -1,2 +1,11 @@
 public class Pedido {
+    private final int id;
+    private Cliente cliente;
+    private ItemPedido[] vetor;
+
+    public Pedido(int id,Cliente cliente,ItemPedido[] vetor) {
+        this.id = id;
+        this.cliente = cliente;
+        this.vetor = vetor;
+    }
 }
